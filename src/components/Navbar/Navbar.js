3fdee5e33ce0281443133ecc4ff309f8921c3fbd
duttprem <premdutt083@gmail.com>
2023-06-11 13:58:55 +0530

    add components

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Navbar.css"
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <h2>Extract Data From CSV Table</h2>
+    </div>
+  )
+}
+
+export default Navbar
